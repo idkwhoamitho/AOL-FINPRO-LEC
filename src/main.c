c62312be73dad2b84ddb4117dca2a5f05a692125
Mike <mike99wazoski@gmail.com>
2025-01-04 18:57:19 +0700

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,75 +2,106 @@
 #include "data_processing.h"
 #include "menu.h"
 #include "fileIO.h"
-//DATA OPERATION ZONE
-
-//FILE IO ZONE
-
-
-//MENU ZONE
-
-
 
 void allmenu(){
-    printProductsCatalogue();
-    int choose = -1;
+    int choice;
     while(1){
-    	Sleep(50);
-        printf("\n		Main Menu\n");
-        printf("\033[33m------------------------------------------\033[0m\n");
-    	Sleep(50);
-        printf("\n1. Sort By ID");
-    	Sleep(50);
-        printf("\n2. Sort By Price");
-    	Sleep(50);
-        printf("\n3. Sort By Stocks");
-    	Sleep(50);
-        printf("\n4. Buy");
-    	Sleep(50);
-        printf("\n5. Add product");
-    	Sleep(50);
-    	printf("\n6. Add existing Products");
-    	Sleep(50);
-    	printf("\n7. Delete product");
-    	Sleep(50);
-        printf("\n0. Exit");
-    	Sleep(50);
-        printf("\nYour choice: ");
-        scanf("%d",&choose);
+    printf("INVENTORY SYSTEM\n");
+    printf("INVENTORY DATA MAIN MENU\n\n");
+    printf("What do you want to do?\n");
+    printf("1. Insert Record\n");
+    printf("2. Show Record\n");
+    printf("3. Select Data\n");
+    printf("4. Delete Record\n");
+    printf("5. Update Record\n");
+    printf("6. Exit\n\n");
+    printf("Your Choice: ");
+    scanf("%d", &choice);
 
-        if(choose == 0){
-        	system("cls || clear"); break;
-		} else if(choose == 2){
-			sortProductByPrice(jumlahProd); system("cls || clear"); printSortedProducts();
-		} else if(choose == 1){
-			sortProductById(jumlahProd); system("cls || clear"); printSortedProducts();
-		} else if(choose == 3){
-			sortProductByStock(jumlahProd); system("cls || clear"); printSortedProducts();	
-		} else if(choose == 4){
-			system("cls || clear"); transactionMenu();
-		} else if(choose == 5){
-			system("cls || clear"); addDataMenu(); system("cls || clear");readFile(); printSortedProducts();
-		} else if(choose == 6){
-			system("cls || clear"); AddExistingDataMenu(); system("cls || clear");readFile(); printSortedProducts();
-		} else if(choose == 7){
-			system("cls || clear"); DeleteDataMenu(); system("cls || clear");readFile(); printSortedProducts();
-		}
+    if(choice == 1){
+        printf("INVENTORY SYSTEM\n");
+        printf("INSERT RECORD MAIN MENU\n\n");
+        printf("1. Iventory\n");
+        printf("2. Supplier\n");
+        printf("3. Item\n");
+        printf("4. Back to Main Menu\n\n");
+        printf("Your Choice: ");
+        scanf("%d", &choose);
 
-    
+        if(choose == 1){
+        	struct Inventory inventoryData;
+            printf("1. Insert the item id: ");
+            scanf("%d",&inventoryData.item_id);
+            printf("2. Insert the supplier id: ");
+            scanf("%d",&inventoryData.supplier_id);
+            printf("3. Insert the quantity: ");
+            scanf("%d",&inventoryData.quantity);
+            addInventory(inventoryData,"./");
+            printf("\nSuccess\n\n");
+            
+        } else if(choose == 2){
+       		struct Supplier supplierData;
+            printf("1. Insert the supplier id: ");
+            scanf("%d",&supplierData.id);
+            printf("2. Insert the supplier name: ");
+            scanf("%s",&supplierData.name);
+            printf("3. Insert the supplier phone number: ");
+            scanf("%s",&supplierData.phone_number);
+            addSupplier(supplierData,"./supplier.dat");
+            printf("\nSuccess\n\n");
+            
+        } else if(choose == 3){
+            struct Item itemData;
+            printf("1. Insert the item id: ");
+            scanf("%d",&itemData.id);
+            printf("2. Insert the item name: ");
+            scanf("%s",&itemData.item_name);
+            printf("3. Insert the price (.00): ");
+            scanf("%f",&itemData.price);
+            printf("3. Insert the quantity: ");
+            scanf("%d",&itemData.quantity);
+        	addRecordMenuItem();
+            addItem(itemData,"./item.dat");
+            printf("\nSuccess\n\n");
+            
+        } else if(choose == 4){
+            continue;
+        }
+        
+    } else if(choice == 2){
+        printf("INVENTORY SYSTEM\n");
+        printf("SHOW RECORD MAIN MENU\n\n");
+        
+        struct Item* items = readFileItem("items.dat");
+		int rows = get_length("items.dat");
+//        printRecordAll(items, rows);
+//        showRecordMenu();
+        
+        
+    } else if(choice == 3){
+        printf("INVENTORY SYSTEM\n");
+        printf("SELECT RECORD MAIN MENU\n\n"); 
+        
+        
+    } else if(choice == 4){
+        printf("INVENTORY SYSTEM\n");
+        printf("DELETE RECORD MAIN MENU\n\n");   
+		DeleteDataMenu();
+		   
+    } else if(choice == 5){
+        printf("INVENTORY SYSTEM\n");
+        printf("UPDATE RECORD MAIN MENU\n\n");      
+		ModifyExistingDataMenu(); 
+		
+		
+    } else if(choice == 6){
+        break;
     }
-    displayLogoend();
+    }
 }
-
-
-
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    system("cls || clear");
-    displayLogo();
-
-    getchar();
     system("cls || clear");
     allmenu();
 
